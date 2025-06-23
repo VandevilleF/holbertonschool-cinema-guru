a37da109220c0e9f5ae704c25fd02887868bff92
VandevilleF <flo.vandeville@gmail.com>
2025-06-23 15:23:42 +0200

    Add Button component with icon support

---
+++ cinema-guru/src/components/general/Button.jsx
@@ -0,0 +1,12 @@
+import './general.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+export default function Button({ label, className, onClick, icon }) {
+	return (
+	<button
+	className={ className }
+	onClick={ onClick }>
+		{icon && <FontAwesomeIcon icon={ icon } />}
+		{ label }</button>
+	)
+}
