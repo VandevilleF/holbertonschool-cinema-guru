2ed7af1200a281a60354c1bff20fc048fa286c7d
VandevilleF <flo.vandeville@gmail.com>
2025-06-24 10:31:30 +0200

    Implement authentication logic and conditional rendering in App component (Task 3)

--- cinema-guru/src/App.jsx
+++ cinema-guru/src/App.jsx
@@ -1,11 +1,45 @@
 import './App.css'
+import { useState, useEffect } from 'react'
+import axios from 'axios';
 
-function App() {
+
+export default function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [userUsername, setUserUsername] = useState("")
+
+  useEffect(() => {
+    const token = localStorage.getItem("accessToken");
+    if (!token) return;
+
+    axios.post('/api/auth/',
+      {},
+      {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      }
+    )
+    .then(response => {
+      setIsLoggedIn(true);
+      setUserUsername(response.data.username);
+    })
+    .catch(error => {
+      console.error(error);
+    })
+  }, {});
+
+
   return (
     <>
-      <div className="App"></div>
+      <div className="App">
+        {isLoggedIn ? (
+          <p>Dashboard component</p>
+        ) : (
+          <p>Authentication component</p>
+        )}
+      </div>
     </>
   )
 }
 
-export default App
+
