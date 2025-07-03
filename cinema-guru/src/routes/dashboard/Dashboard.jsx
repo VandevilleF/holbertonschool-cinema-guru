53787fe66ba564a6052c784ca54121f805b1d0fa
VandevilleF <flo.vandeville@gmail.com>
2025-07-03 14:49:40 +0200

    Wrap Dashboard component in BrowserRouter and add routing for home, favorites, and watch later pages (task 8)

--- cinema-guru/src/routes/dashboard/Dashboard.jsx
+++ cinema-guru/src/routes/dashboard/Dashboard.jsx
@@ -1,12 +1,21 @@
 import './dashboard.css'
 import Header from '../../components/navigation/Header'
 import SideBar from '../../components/navigation/SideBar'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 
 export default function Dashboard({ userUsername, setIsLoggedIn }) {
 	return (
-	<div className='header'>
-		<Header userUsername={userUsername} setIsLoggedIn={setIsLoggedIn} />
-		<SideBar />
-	</div>
+		<BrowserRouter>
+			<div className='header'>
+				<Header userUsername={userUsername} setIsLoggedIn={setIsLoggedIn} />
+				<SideBar />
+			</div>
+		<Routes>
+			<Route path='/home' element={HomePage} />
+			<Route path='/favorites' element={Favorites} />
+			<Route path='/watchlater' element={WhatchLater} />
+			<Route path='*' element={<Navigate to="/home" />} />
+		</Routes>
+		</BrowserRouter>
 	)
 }
